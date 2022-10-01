73b6b7aa5f031ccf4ad5a655e306a78154aed4a3
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-10-01 17:06:06 +0530

    Adding new Contacts

--- addressbook/AddressBookMain.java
+++ addressbook/AddressBookMain.java
@@ -2,11 +2,11 @@ package com.bridgelabz.addressbook;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		
+
 		System.out.println("***** WELCOME TO ADDRESS BOOK ******");
-		
+
 		AddressBook addressBook = new AddressBook();
-		
+
 		addressBook.addContact();
 	}
 }
