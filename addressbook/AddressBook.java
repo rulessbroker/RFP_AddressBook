76bd6435a487d71abbe7d8220c99901abf7dbe9d
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-10-01 17:39:01 +0530

    Delete Person Using Person Name

--- addressbook/AddressBook.java
+++ addressbook/AddressBook.java
@@ -112,4 +112,24 @@ public class AddressBook {
 			System.out.println("Contact not found!!!");
 	}
 
+	public void deleteContact() {
+
+		System.out.println("Enter the person whose contact to be deleted :");
+		System.out.println("Enter First Name:");
+		String fname = sc.nextLine();
+		System.out.println("Enter Last Name:");
+		String lname = sc.nextLine();
+
+		for (int j = 0; j < people.size(); j++) {
+			Contact person = people.get(j);
+			if (person.firstName.equals(fname) && person.lastName.equals(lname)) {
+				people.remove(person);
+				System.out.println("person removed sucessfully");
+			} else {
+				System.out.println("contact not matched");
+			}
+		}
+
+	}
+
 }
--- addressbook/AddressBookMain.java
+++ addressbook/AddressBookMain.java
@@ -10,29 +10,29 @@ public class AddressBookMain {
 		AddressBook addressBook = new AddressBook();
 		int choice = 0;
 		Scanner input = new Scanner(System.in);
-		
+
 		do {
 			System.out.println("Enter your choice --->\n");
-			System.out.println("1.Add Contact\n" + "2.Edit Contact\n" + "3.Exit\n");
+			System.out.println("1.Add Contact\n" + "2.Edit Contact\n" + "3.Delete Contact\n" + "4.Exit\n");
 			choice = input.nextInt();
-			
-			switch(choice) {
-			case 1: 
+
+			switch (choice) {
+			case 1:
 				addressBook.addContact();
 				break;
-			case 2: 
+			case 2:
 				addressBook.editContact();
 				break;
 			case 3:
+				addressBook.deleteContact();
+			case 4:
 				System.out.println("* Successfully Exit *");
 				break;
 			default:
 				System.out.println("Enter Valid Input !!!\n");
 			}
-			
-		}while(choice != 3);
-		
-		
-		
+
+		} while (choice != 4);
+
 	}
 }
